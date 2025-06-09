3e728b8c89ff07315b17972e061530642d993c1c
Yakub Firman mustofa <113831481+yakubfirmanmustofa@users.noreply.github.com>
2025-06-09 19:09:25 +0700

    hehe

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->replace(
+            Illuminate\Http\Middleware\TrustProxies::class,
+            App\Http\Middleware\TrustProxies::class
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
